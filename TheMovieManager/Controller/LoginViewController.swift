fe54cd0eaceee07a3ea6c252b3691f2658d464f7
AlekseyK <aleksey.kabishau@gmail.com>
2019-05-13 22:31:49 -0400

    Initial Commit

---
+++ TheMovieManager/Controller/LoginViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginViaWebsiteButton: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        emailTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "completeLogin", sender: nil)
+    }
+    
+    @IBAction func loginViaWebsiteTapped() {
+        performSegue(withIdentifier: "completeLogin", sender: nil)
+    }
+    
+}
