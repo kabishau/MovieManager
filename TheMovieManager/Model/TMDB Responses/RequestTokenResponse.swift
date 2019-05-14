2b23c48cbaad627533de80b04e84320f884d3b67
AlekseyK <aleksey.kabishau@gmail.com>
2019-05-13 23:52:41 -0400

    struct RequestTokenResponse to match the response from temporary token request

--- TheMovieManager/Model/TMDB Responses/RequestTokenResponse.swift
+++ TheMovieManager/Model/TMDB Responses/RequestTokenResponse.swift
@@ -1,3 +1,19 @@
 import Foundation
 
+struct RequestTokenResponse: Codable {
+    
+    let success: Bool
+    let expiresAt: String
+    let requestToken: String
+    
+    enum CodingKeys: String, CodingKey {
+        case success
+        case expiresAt = "expires_at"
+        case requestToken = "request_token"
+    }
+
+    //"success": true,
+    //"expires_at": "2019-05-14 04:12:52 UTC",
+    //"request_token": "eacad2d87559b90950fd646982f44c9e522ee40e"
+}
 
