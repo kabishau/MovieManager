0f6d5d477e8a4de3230aede59faf6f04a3dfe558
Aleksey DreamLine <aleksey.kabishau@dreamline.com>
2019-05-15 13:05:53 -0400

    created class func to make request for session id

--- TheMovieManager/Model/TMDB Client/TMDBClient.swift
+++ TheMovieManager/Model/TMDB Client/TMDBClient.swift
@@ -98,4 +98,28 @@ class TMDBClient {
         }
         task.resume()
     }
+    
+    class func getSessionId(completion: @escaping (Bool, Error?) -> Void) {
+        
+        var request = URLRequest(url: Endpoints.sessionId.url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let body = PostSession(requestToken: Auth.requestToken)
+        request.httpBody = try! JSONEncoder().encode(body)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {
+                completion(false, error)
+                return
+            }
+            do {
+                let responseObject = try JSONDecoder().decode(SessionResponse.self, from: data)
+                self.Auth.sessionId = responseObject.sessionId
+                completion(true, nil)
+            } catch {
+                completion(false, error)
+            }
+        }
+        task.resume()
+    }
 }
